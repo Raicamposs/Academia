d3a52190ea9f6857d2165dd7d20112ebd578b76f
Costa <cameroon_export@hotmail.com>
2015-05-22 22:47:59 -0300

    Criação das Classes Estado, EstadoMatricula, EstadoMensalidade, Função.

---
+++ Gerenciador/src/Matricula/EstadoMensalidade.java
@@ -0,0 +1,58 @@
+package Matricula;
+
+
+public class EstadoMensalidade {
+
+    /**
+     * Método construtor. Você deve utiliza-lo para criar o registro de um novo
+     * author .
+     *
+     * @param estadoMensalidadeId id da EstadoMensalidade
+     * @param estadoMensalidadeNome nome da EstadoMensalidade
+     * @author 
+     */
+    private int estadoMensalidadeId;
+    private String estadoMensalidadeNome;
+
+    // Declaração do construtor da classe estadoMensalidade//
+    public EstadoMensalidade(int estadoMensalidadeId, String estadoMensalidadeNome) {
+
+        this.estadoMensalidadeId = estadoMensalidadeId;
+        this.estadoMensalidadeNome = estadoMensalidadeNome;
+    }
+// Declaração do construtor da classe estadoMensalidade//
+
+    public EstadoMensalidade(String estadoMensalidadeNome) {
+
+        this.estadoMensalidadeNome = estadoMensalidadeNome;
+    }
+
+    //Declaração do método estadoMensalidadeId
+
+    public void setEstadoMensalidadeId(int estadoMensalidadeId) {
+        this.estadoMensalidadeId = estadoMensalidadeId;
+        // Altera a variavel estadoMensalidadeId da classe EstadoMensalidade para o parametro passado
+    }
+
+    //Declaração do método setEstadoMensalidadeNome
+
+    public void setEstadoMensalidadeNome(String estadoMensalidadeNome) {
+        this.estadoMensalidadeNome = estadoMensalidadeNome;
+        // Altera a variavel estadoMensalidadeNome da classe EstadoMensalidade para o parametro passado
+    }
+
+    //Declaração do método getEstadoMensalidadeId 
+
+    public int getEstadoMensalidadeId() {
+        return estadoMensalidadeId;
+        //retorna o Valor da Variavel  estadoMensalidadeId
+    }
+
+    //Declaração do método getEstadoMensalidadeNome
+
+    public String getEstadoMensalidadeNome() {
+        return estadoMensalidadeNome;
+        //retorna o Valor da Variavel  estadoMensalidadeNome
+    }
+
+}
---
+++ Gerenciador/src/Objetos/Alunos/Estado.java
@@ -0,0 +1,71 @@
+package Objetos.Alunos;
+
+public class Estado {
+
+    /**
+     * Método construtor. Você deve utiliza-lo para criar o registro de um novo
+     * author.
+     *
+     * @param estadoId id do Estado
+     * @param estadoNome nome do Estado
+     * @author 
+     */
+    private int estadoId;
+    private String estadoNome;
+    private String estadoUf;
+
+    // Declaração do construtor da classe estado//
+    public Estado(int estadoId, String estadoNome, String estadoUf) {
+
+        this.estadoId = estadoId;
+        this.estadoNome = estadoNome;
+        this.estadoUf = estadoUf;
+    }
+// Declaração do construtor da classe estado//
+
+    public Estado(String estadoNome) {
+
+        this.estadoNome = estadoNome;
+    }
+
+    //Declaração do método estadoId
+
+    public void setEstadoId(int estadoId) {
+        this.estadoId = estadoId;
+        // Altera a variavel estadoId da classe Estado para o parametro passado
+    }
+
+    
+    //Declaração do método setEstadoNome
+
+    public void setEstadoUf(String estadoUf) {
+        this.estadoUf = estadoUf;
+        // Altera a variavel estadoUf da classe Estado para o parametro passado
+    }
+
+    //Declaração do método getEstadoId 
+
+    public int getEstadoId() {
+        return estadoId;
+        //retorna o Valor da Variavel  estadoId
+    }
+
+    //Declaração do método getEstadoNome
+
+    public String getEstadoNome() {
+        return estadoNome;
+        //retorna o Valor da Variavel  estadoNome
+        
+    }
+               
+    //Declaração do método getEstadoUf
+
+    public String getEstadoUf() {
+        return estadoUf;
+        //retorna o Valor da Variavel  estadoUf
+        
+     
+        
+    }
+
+}
