2387397906e3346fc09cb1e81f1a52db8ff87403
Wagnerlemos33 <wagnerlemos7@gmail.com>
2015-05-24 21:14:09 -0300

    Modelagem do Banco

---
+++ Gerenciador/src/Banco/Telefone.sql
@@ -0,0 +1,6 @@
+create table telefone (
+tel_id Integer Primary Key Not Null Unique Auto_Increment, 
+tel_num integer (11) Not Null, 
+tel_pes_id integer  not null References `pessoa`(pes_id)
+
+); 
