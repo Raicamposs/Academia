f6bc986bc1274663372c50860559f45dc4051703
Wagnerlemos33 <wagnerlemos7@gmail.com>
2015-05-30 18:55:51 -0300

    Modelagem do banco

---
+++ Gerenciador/src/Banco/InsertDiasDaSemana.sql
@@ -0,0 +1,8 @@
+insert into dias_semana( dia_nome) values ('segunda');
+insert into dias_semana( dia_nome) values ('terca');
+insert into dias_semana( dia_nome) values ('quarta');
+insert into dias_semana( dia_nome) values ('quinta');
+insert into dias_semana( dia_nome) values ('sexta');
+insert into dias_semana( dia_nome) values ('sabado');
+insert into dias_semana( dia_nome) values ('domingo');
+
