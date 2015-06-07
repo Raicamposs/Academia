9afae55dee734ca2dc2f2b36661b2b226e9ea716
RAIANE CAMPOS JANUARIO <raianejanu@gmail.com>
2015-06-07 16:37:59 -0300

    Modelagem banco: alteração tabela Endereço

--- Gerenciador/src/Banco/Endereco.sql
+++ Gerenciador/src/Banco/Endereco.sql
@@ -1,17 +1,14 @@
+
 create table endereco (
   end_id integer unsigned not null auto_increment,
-  end_rua_cep integer unsigned not null,
-  end_est_uf varchar(2) not null,
-  end_cid_id integer unsigned not null,
+  end_rua_cep integer unsigned not null references rua(rua_cep),
+  end_complemento varchar(100) null,
   end_num varchar(8) null,
   primary key(end_id),
-  index endereco_fkindex1(end_cid_id),
-  index endereco_fkindex3(end_est_uf),
-  index endereco_fkindex4(end_rua_cep),
-  foreign key(end_cid_id) references cidade(cid_id),
-  foreign key(end_est_uf) references estado(est_uf),
-  foreign key(end_rua_cep) references rua(rua_cep)
+  index endereco_fkindex(end_rua_cep)
+
 );
+
 
 
 
