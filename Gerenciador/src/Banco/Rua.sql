6943b9f9a8a9e1525098700c889cadfa36d131e2
RAIANE CAMPOS JANUARIO <raianejanu@gmail.com>
2015-06-07 16:24:23 -0300

    Modelagem banco: alteração tabela rua

--- Gerenciador/src/Banco/Rua.sql
+++ Gerenciador/src/Banco/Rua.sql
@@ -1,8 +1,10 @@
 
+
 create table rua (
-  rua_cep integer unsigned not null,
-  rua_nome varchar unsigned not null unique,
+  rua_cep int(10)  not null,
+  rua_nome varchar(45) not null ,
   rua_id_bairro integer unsigned not null references `bairro`(bai_id),
   primary key(rua_cep)
 );
+
 
