ffdee2499d4337bc3107bcee0217d5556fa99873
RAIANE CAMPOS JANUARIO <raianejanu@gmail.com>
2015-06-07 16:50:18 -0300

    Modelagem banco: alteração tabela pessoa

--- Gerenciador/src/Banco/Pessoa.sql
+++ Gerenciador/src/Banco/Pessoa.sql
@@ -1,16 +1,15 @@
 create table pessoa (
-pes_id Integer Primary Key Not Null Unique Auto_Increment,
+pes_id Integer Not Null Unique Auto_Increment,
 pes_nome Varchar (45) Not Null,
-pes_rg Varchar (10)  Not Null ,
-pes_cpf Varchar (11)  Not Null , 
-pes_data_nascimento date Not Null, 
-pes_email Varchar (30)  Not Null , 
+pes_rg Varchar (10)  Not Null Unique ,
+pes_cpf Varchar (11)  Not Null Unique,
+pes_data_nascimento date Not Null,
+pes_email Varchar (30)  Not Null Unique ,
 pes_sexo char(1) Not Null ,
-  pes_end_id integer unsigned not null,
- pes_ec_id integer unsigned not null,
-  primary key(pes_cod, pes_rg, pes_cpf),
-  index pessoa_fkindex1(pes_cpf),
-  index pessoa_fkindex2(pes_nome),
-  foreign key( pes_end_id) references endereco(end_id),
-  foreign key(pes_ec_id) references estado_civil(ec_id)
+pes_end_id integer unsigned not null references endereco(end_id),
+pes_ec_id integer unsigned not null references estado_civil(ec_id),
+primary key(pes_id, pes_rg, pes_cpf),
+index pessoa_fkindex1(pes_cpf),
+index pessoa_fkindex2(pes_nome)
+
 )
